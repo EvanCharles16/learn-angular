1ad5c169e29a4669d27d14506a82bc6b2f64eeb9
EvanCharles16 <evancharles1609@gmail.com>
2020-07-15 12:58:52 +0700

    assignment 1 : Adding warning & success components with styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+// import { ServerComponent } from './server/server.component';
+// import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent],
