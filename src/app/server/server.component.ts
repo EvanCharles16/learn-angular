8b8d8f0e98ddd15c5ca5f68226d1a937c8a2150c
EvanCharles16 <evancharles1609@gmail.com>
2020-07-16 14:01:42 +0700

    add getColor func for the message

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,12 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+        font-weight: bold;
+        padding: 20px;
+        border: 1px solid black;
+        font-size: 16px;
+      }
+      .offline {
+        color: white;
+        font-weight: bold;
+        padding: 20px;
+        border: 1px solid black;
+        font-size: 16px;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   serverId: number = 10;
-  serverStatus: string = 'Offline';
+  serverStatus: string = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
